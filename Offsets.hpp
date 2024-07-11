7d3edbda7d647e4cafb0ce3b4292d63b19c5f83d
Matt <xz78zx@aim.com>
2024-07-11 15:20:12 -0400

    update

--- Offsets.hpp
+++ Offsets.hpp
@@ -2,20 +2,20 @@
 
 namespace OW {
 	namespace offset {
-		static constexpr auto Address_viewmatrix_base = 0x372C110; //viewmatrix xor 110 or 170?
-		static constexpr auto Address_viewmatrix_base_test = 0x3DEB808; //viewmatrix 
-		static constexpr auto Address_entity_base = 0x3717600; //entityadmin/list 
-		static constexpr auto offset_viewmatrix_ptr = 0x7E0; //pointer
-		static constexpr auto offset_viewmatrix_xor_key = 0x1603E7436E47D0CA; //vm key 
-		static constexpr auto HeapManager = 0x37E5260; //heapmanagervar 
-		static constexpr auto HeapManager_Var = 0x37CA0AE; //heapmanager 
-		static constexpr auto HeapManager_Key = 0x6E06BCEBE3CE3478; // 
-		static constexpr auto HeapManager_Pointer = 0x160; //
-		static constexpr auto changefov = 0x388F7A8; //myFOV 
-		static constexpr auto SensitivePtr = 0x205C; //old 2054
-		static constexpr auto VisFN = 0x7A6392; // *
-		static constexpr auto Vis_Key = 0x5AE142A313A92858; //same for function
-		static constexpr auto OutlineFN = 0x7AFA72; // *
-		static constexpr auto Outline_Key = 0x6847903E1BDE5A37; //
+		static constexpr auto Address_viewmatrix_base = 0x369AAC0; //viewmatrix xor *
+		static constexpr auto Address_viewmatrix_base_test = 0x3D5D618; //viewmatrix  *
+		static constexpr auto Address_entity_base = 0x3685F90; //entityadmin/list *
+		static constexpr auto offset_viewmatrix_ptr = 0x7E0; //pointer 
+		static constexpr auto offset_viewmatrix_xor_key = 0xBBE9AC19BDDDC4B0; //vm key *
+		static constexpr auto HeapManager = 0x3753420; //heapmanagervar *
+		static constexpr auto HeapManager_Var = 0x3737619; //heapmanager *
+		static constexpr auto HeapManager_Key = 0xC364D216A68B6134; // *
+		static constexpr auto HeapManager_Pointer = 0x160; // *
+		static constexpr auto changefov = 0x37FE348; //myFOV *
+		static constexpr auto SensitivePtr = 0x2064; // *
+		static constexpr auto VisFN = 0x79DA42; // *
+		static constexpr auto Vis_Key = 0x2F8E798891387671; // *
+		static constexpr auto OutlineFN = 0x7A6752; // *
+		static constexpr auto Outline_Key = 0x6D73243BD7E1019E; // *
 	}
 }
